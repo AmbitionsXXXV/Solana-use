b105356c28df51d048f8315235cc3308efb157e7
etctera <yilonglei790@gmail.com>
2024-01-10 15:15:25 +0800

    feat: 通过运行时传递参数检查连接，并获取账户余额

---
+++ transfer.ts
@@ -0,0 +1,47 @@
+import { getKeypairFromEnvironment } from '@solana-developers/node-helpers'
+import {
+  Connection,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  sendAndConfirmTransaction,
+} from '@solana/web3.js'
+import 'dotenv/config'
+
+const suppliedToPubkey = process.argv[2] || null
+
+if (!suppliedToPubkey) {
+  console.log(`Please provide a public key to send to`)
+  process.exit(1)
+}
+
+const senderKeypair = getKeypairFromEnvironment('SECRET_KEY')
+
+console.log(`suppliedToPubkey: ${suppliedToPubkey}`, senderKeypair)
+
+const toPubkey = new PublicKey(suppliedToPubkey)
+
+const connection = new Connection('https://api.devnet.solana.com', 'confirmed')
+
+console.log(
+  `✅ Loaded our own keypair, the destination public key, and connected to Solana`,
+)
+
+const transaction = new Transaction()
+
+const LAMPORTS_TO_SEND = 5000
+
+const sendSolInstruction = SystemProgram.transfer({
+  fromPubkey: senderKeypair.publicKey,
+  toPubkey,
+  lamports: LAMPORTS_TO_SEND,
+})
+
+transaction.add(sendSolInstruction)
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+  senderKeypair,
+])
+
+console.log(`💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `)
+console.log(`Transaction signature is ${signature}!`)
