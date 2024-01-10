a2a7f1399fbd935fde3be31f23033af58b854182
etctera <yilonglei790@gmail.com>
2024-01-10 14:25:51 +0800

    feat: 生成 sol 公私钥对

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,9 +1,13 @@
 import { Keypair } from '@solana/web3.js'
+import 'dotenv/config'
 
 /**
  * 生成 sol 公私钥对
  */
 const keypair = Keypair.generate()
 
+console.log(`✅ Generated keypair!`)
+
 console.log(`The public key is: `, keypair.publicKey.toBase58())
 console.log(`The secret key is: `, keypair.secretKey)
+console.log(`✅ Finished!`)
