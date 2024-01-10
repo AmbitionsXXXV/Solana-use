bc5f2b2f76dc5c9fb769d917b9dfdcc09f436592
etctera <yilonglei790@gmail.com>
2024-01-10 14:27:41 +0800

    feat: 从环境变量中获取 sol SECRET_KEY

--- load-keypair.ts
+++ load-keypair.ts
@@ -1,6 +1,9 @@
 import { getKeypairFromEnvironment } from '@solana-developers/node-helpers'
 import 'dotenv/config'
 
+/**
+ * 从环境变量中获取 sol SECRET_KEY
+ */
 const keypair = getKeypairFromEnvironment('SECRET_KEY')
 
 console.log(
