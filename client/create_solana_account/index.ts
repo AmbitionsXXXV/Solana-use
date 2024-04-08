b261ec9a1b647f8f22be3963d1316a870929c767
etctera <yilonglei790@gmail.com>
2024-04-08 22:40:54 +0800

    feat: decode secret key & create account

--- client/create_solana_account/index.ts
+++ client/create_solana_account/index.ts
@@ -4,7 +4,7 @@ import { promises as fs } from 'fs' // 导入fs模块的promises API，用于异
 import path from 'path' // 导入path模块，用于处理文件和目录的路径
 
 // 定义批量生成账户的数量
-const NUM_ACCOUNTS = 10 // 你可以根据需要调整这个值
+const NUM_ACCOUNTS = 4 // 你可以根据需要调整这个值
 
 /**
  * 批量生成 Solana 基本账户并保存到 accounts.json 文件
@@ -21,6 +21,11 @@ const generateAndSaveAccounts = async (numAccounts: number) => {
       publicKey: keypair.publicKey.toBase58(), // 公钥用于接收资金，使用 toBase58 方法转换为 Base58 格式，这是 Solana 公钥的常见表示方式
       secretKey: Array.from(keypair.secretKey), // 私钥用于签名交易，是一个 Uint8Array，使用 Array.from 方法转换为数组，以便于 JSON 序列化
     })
+
+    await fs.writeFile(
+      path.resolve(__dirname, `./key${i}.json`),
+      JSON.stringify(Array.from(keypair.secretKey)), // 使用 JSON.stringify 方法将 accounts 数组转换为JSON格式的字符串，JSON.stringify 的第二个和第三个参数用于格式化输出，使得生成的 JSON 文件更易读
+    )
   }
 
   try {
